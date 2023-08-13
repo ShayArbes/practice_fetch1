d65ed1035ffb562ef098f62d782c1ebab4dbd718
Shay Arbes <shayarbes1@gmail.com>
2023-08-13 15:31:07 +0300

    1.2

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
-async function fetchData() {
+async function fetchData(url) {
   try {
-    const response = await fetch("https://randomuser.me/api");
+    const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      return data.results[0];
+      return data.results;
     } else {
       throw new Error("error1");
     }
@@ -13,15 +13,14 @@ async function fetchData() {
 }
 
 const createNewPerson = (person) => {
-const userInformation = document.querySelector("#user_information");
-userInformation.innerHTML ="";
+  const userInformation = document.querySelector("#user_information");
   console.log(person);
   const name = document.createElement("h2");
-  name.innerText ="name: " + person.name.first;
+  name.innerText = "name: " + person.name.first;
   const emil = document.createElement("h2");
-  emil.innerText ="emil: " + person.email;
+  emil.innerText = "emil: " + person.email;
   const age = document.createElement("h2");
-  age.innerText ="age: " + person.dob.age;
+  age.innerText = "age: " + person.dob.age;
   const img = document.createElement("img");
   img.src = person.picture.medium;
 
@@ -29,17 +28,31 @@ userInformation.innerHTML ="";
   userInformation.appendChild(emil);
   userInformation.appendChild(age);
   userInformation.appendChild(img);
-
 };
 
 const getRandomPerson = async () => {
-  const randomPerson = await fetchData();
-  createNewPerson(randomPerson);
+  const userInformation = document.querySelector("#user_information");
+  userInformation.innerHTML = "";
+  const randomPerson = await fetchData("https://randomuser.me/api");
+  createNewPerson(randomPerson[0]);
+};
+
+const getFiveMales = async () => {
+  const userInformation = document.querySelector("#user_information");
+  userInformation.innerHTML = "";
+  const randomPerson = await fetchData(
+    "https://randomuser.me/api/?results=5&gender=male"
+  );
+  randomPerson.forEach((element) => {
+    createNewPerson(element);
+  });
 };
 
 const init = () => {
   const btn = document.querySelector("#btn");
+  const btnFiveMales = document.querySelector("#btnFiveMales");
   btn.addEventListener("click", getRandomPerson);
+  btnFiveMales.addEventListener("click", getFiveMales);
 };
 
 init();
